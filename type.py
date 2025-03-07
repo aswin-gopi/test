c18437fce739c3b189d7aad443a7b12828ca3306
aswin gopi <gopiaswin917@gmail.com>
2025-03-07 14:59:33 +0530

    added type.py file

---
+++ type.py
@@ -0,0 +1,42 @@
+# basic printing statement
+# print("hello, world")
+
+
+# using string concatinate
+# print("hello, "+name)
+
+# using f string
+# name = "bob"
+# print (f"hello, {name}")
+
+#using str.format
+#name = "Alice"
+#print("Hello {}!".format(name))
+
+
+#printing multiple arguement
+# name = "aswin"
+# age = 21
+# print("name:",name, "age:",age)
+
+
+#using sep to seperate
+# name = "hello"
+# age = 21
+# print("name:",name, "age:",age, sep="|")
+
+
+#end to customer
+# print("hello", end=" ")
+# print("world")
+
+#tag
+# print("hello\nworld")
+# print("hello\tworld")
+
+
+# loop
+# for i in range(3):
+#     print(f"Message {i}")
+
+
